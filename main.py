2c2c612d2befa898d3945bf5ae40fbdda428256e
Dainokode <antoniodainok@gmail.com>
2020-12-21 11:20:24 +0100

    create Snake class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 
@@ -10,24 +11,8 @@ screen.tracer(0)
 screen.title("Play Snake")
 
 
-# snake individual parts stored in a list - start position of each part
-snake_parts = []
-snake_parts_coordinates = [-20, -30, 0]
-
-
-# create a new object for each number in range func and add it to snake_parts list
-for snake_index in range(1, 4):
-    new_snake_part = Turtle()
-    new_snake_part.penup()
-    new_snake_part.color("white")
-    new_snake_part.shape("square")
-    new_snake_part.shapesize(1.5)
-    snake_parts.append(new_snake_part)
-
-
-# each snake part gets moved to a random coordinate chosen from snake_parts_coordinates and correctly positioned in the middle of the screen
-for snake_part in range(len(snake_parts)):
-    snake_parts[snake_part].goto(snake_parts_coordinates[snake_part], 0)
+# init snake object from Snake class
+snake = Snake()
 
 
 is_game_on = True
@@ -36,14 +21,9 @@ while is_game_on:
     screen.update()
     # snake moves slower
     time.sleep(0.1)
-    # for each snake part in the snake_parts list move the last part coordinates to the first and the first part forward by 20px
-    for snake_part in range(len(snake_parts)- 1, 0, -1):
-        new_x = snake_parts[snake_part - 1].xcor()
-        new_y = snake_parts[snake_part - 1].ycor()
-        snake_parts[snake_part].goto(new_x, new_y)
-    snake_parts[0].forward(20)
+    # call move func from Snake class
+    snake.move()
+
 
         
-
-
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+
+
+# CONSTANT VARIABLES:
+
+# snake individual parts stored in a list(snake_parts) - start position of each part
+SNAKE_COORDINATES = [-20, -30, 0]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.snake_parts = []
+        self.create_snake()
+
+    def create_snake(self):
+        # create a new object for each number in range func and add it to snake_parts list
+        for snake_index in range(1, 4):
+            new_snake_part = Turtle()
+            new_snake_part.penup()
+            new_snake_part.color("white")
+            new_snake_part.shape("square")
+            new_snake_part.shapesize(1.5)
+            new_snake_part.goto(SNAKE_COORDINATES[snake_index - 1], 0)
+            self.snake_parts.append(new_snake_part)
+
+    
+    def move(self):
+        # for each snake part in the snake_parts list move the last part coordinates to the first and the first part forward by 20px
+        for snake_part in range(len(self.snake_parts)- 1, 0, -1):
+            new_x = self.snake_parts[snake_part - 1].xcor()
+            new_y = self.snake_parts[snake_part - 1].ycor()
+            self.snake_parts[snake_part].goto(new_x, new_y)
+        self.snake_parts[0].forward(MOVE_DISTANCE)
